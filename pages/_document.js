bd0051dcfc1e5ed1058cae7371a2d8a926c7989e
rmzlb <ramzi.laieb@gmail.com>
2023-02-07 16:48:47 +0100

    feat-carbonable-learn

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Carbz Labs" key="title"/>
+        <meta property="og:description" content="Carbonable Labs " key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://carbonable.io/assets/images/social/social.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
+        <link href="/dist/output.css" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
